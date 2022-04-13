b8483cd70ee70afebd34ab17a2cb27b3885da746
Lucas Silva <maniarenu@hotmail.com>
2022-04-13 20:22:33 -0300

    Atividade PHP

---
+++ Atv1.php
@@ -0,0 +1,15 @@
+<?php
+$nome= "Lucas Ferreira da Silva";
+$Ra=  321105397;
+$nota1=10;
+$nota2=9;
+$nota3=9;
+$media= ($nota1+$nota2+$nota3)/3;
+
+echo  "Nome : $nome "."<br>". "R.A : " . $Ra ."<br>". "Nota 1 : ". $nota1 ."<br>". "Nota 2 : " .$nota2 ."<br>"."Nota 3 :". $nota3 ."<br>". "Média Final : " . $media . "<br>";
+
+if($media>7){
+    echo "Você Foi Aprovado na Materia!";
+} else {
+    echo " Você Foi Reprovado na Materia!";
+}
