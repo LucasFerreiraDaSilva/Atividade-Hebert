a6cd68b1bb9e6b20db5e15f6de130359105112ca
Lucas Silva <maniarenu@hotmail.com>
2022-04-13 20:28:09 -0300

    Atividade PHP

--- Aprovado.php
+++ Aprovado.php
@@ -2,13 +2,13 @@
 $nome= "Lucas Ferreira da Silva";
 $Ra=  321105397;
 $nota1=10;
-$nota2=9;
-$nota3=9;
+$nota2=8;
+$nota3=7;
 $media= ($nota1+$nota2+$nota3)/3;
 
 echo  "Nome : $nome "."<br>". "R.A : " . $Ra ."<br>". "Nota 1 : ". $nota1 ."<br>". "Nota 2 : " .$nota2 ."<br>"."Nota 3 :". $nota3 ."<br>". "Média Final : " . $media . "<br>";
 
-if($media>7){
+if($media>=6){
     echo "Você Foi Aprovado na Materia!";
 } else {
     echo " Você Foi Reprovado na Materia!";
